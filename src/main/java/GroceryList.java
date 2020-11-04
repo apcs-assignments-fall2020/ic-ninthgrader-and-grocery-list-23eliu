a77588d9adeccbfe76246a12af7709cf76ada6db
23eliu <23eliu@athenian.org>
2020-11-04 01:03:52 +0000

    finished homework

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,14 +1,13 @@
-import java.util.Arrays;
-
 public class GroceryList {
     private String[] arr;
 
     public GroceryList() {
-        arr = new String[10];
+        // our grocery list will have a max size of 10
+        this.arr = new String[10]; 
     }
 
     public void add(String item) {
-        for (int i = 0; i < arr.length; i ++) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] == null) {
                 arr[i] = item;
                 break;
@@ -16,7 +15,29 @@ public class GroceryList {
         }
     }
 
+    public void remove(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item) {
+                arr[i] = "";
+            }
+        }
+    }
+
     public String toString() {
-        return Arrays.toString(arr);
+        String str = "Grocery List: ";
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != "") {
+                str += (arr[i] + " ");
+            }
+            else {
+                count++;
+            }
+        }
+        while (count > 0) {
+            str += "null";
+            count--;
+        }
+        return str;
     }
 }
